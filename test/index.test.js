cfb1d70334c40cc1131b257305f338041f8f50e9
onedionys <onedionys@gmail.com>
2024-03-08 12:40:47 +0700

    chore: add test package before publishing

---
+++ test/index.test.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const AuthenticationTokensManager = require('../src/authenticationTokensManager');
+
+describe('AuthenticationTokensManager', function() {
+    describe('#generateToken()', function() {
+        it('should generate a valid token', function() {
+            // Test implementation
+        });
+    });
+
+    describe('#verifyToken()', function() {
+        it('should return true for a valid token', function() {
+            // Test implementation
+        });
+
+        it('should return false for an invalid token', function() {
+            // Test implementation
+        });
+    });
+
+    describe('#refreshTokens()', function() {
+        it('should refresh expired tokens', function() {
+            // Test implementation
+        });
+    });
+});
