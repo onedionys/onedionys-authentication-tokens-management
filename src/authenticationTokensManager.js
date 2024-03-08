b9260c757c18c983179e38a250d0047b6ccfa64a
onedionys <onedionys@gmail.com>
2024-03-08 12:40:45 +0700

    chore: initialize function project

---
+++ src/authenticationTokensManager.js
@@ -0,0 +1,19 @@
+class AuthenticationTokensManager {
+    constructor() {
+        // Constructor implementation
+    }
+
+    generateToken(user) {
+        // Method to generate authentication token for a user
+    }
+
+    verifyToken(token) {
+        // Method to verify if the token is valid
+    }
+
+    refreshTokens() {
+        // Method to refresh expired tokens
+    }
+}
+
+module.exports = AuthenticationTokensManager;
